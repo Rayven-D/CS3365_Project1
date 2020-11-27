7f82598ebc4eca2c6027bfde5f101f80e9c6fe07
Rayven Deray <Rayven.Deray@ttu.edu>
2020-11-27 14:36:15 -0600

    CEO controller and interface

---
+++ HealthCare/src/healthcare/CEOInterface.java
@@ -0,0 +1,50 @@
+//CS 2365 OOP Spring 2020 Section 001
+//Rayven Jan B. Deray
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package healthcare;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.Event;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author rjder
+ */
+public class CEOInterface extends Application {
+    
+    Label dateLabel, numPatServedLabel, amountEarnedLabel;
+    TextField dateField, numPatServedField, amountEarnedField;
+    ComboBox docCombo;
+    
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        
+      
+    }
+    
+    public void handle(Event e){
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
